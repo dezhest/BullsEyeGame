5809307e039f4886de9d325a323a995d75debbfe
dezhest <dezhest@yandex.ru>
2023-04-29 16:48:56 +0400

    constraits

--- swiftapps/ViewController.swift
+++ swiftapps/ViewController.swift
@@ -10,9 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     var model = Model()
     
-    @IBOutlet var slider: UISlider!
-    @IBOutlet var targetLabel: UILabel!
     
+    let targetLabel = UILabel()
     let scoreCounter = UILabel()
     let scoreLabel = UILabel()
     let roundLabel = UILabel()
@@ -21,7 +20,12 @@ class ViewController: UIViewController {
     let goID = UIButton()
     let identifierButton = UIButton()
     let startOverButton = UIButton()
-
+    let slider = UISlider()
+    
+    let label1 = UILabel()
+    let label100 = UILabel()
+    let putYheBulls = UILabel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +33,11 @@ class ViewController: UIViewController {
         hitMe.addTarget(self, action: #selector(showAlert), for: .touchUpInside)
         goID.addTarget(self, action: #selector(navigateByID), for: .touchUpInside)
         identifierButton.addTarget(self, action: #selector(switchWithIdentifier), for: .touchUpInside)
+        slider.addTarget(self, action: #selector(sliderMoved(_:)), for: .touchDragInside)
         startNewRoundView()
         buttonModifire()
         labelModefire()
+        sliderModifire()
         addConstraints()
     }
     @objc func navigateByID() {
@@ -54,7 +60,7 @@ class ViewController: UIViewController {
         model.scoreCalculate()
         startNewRoundView()
     }
-    @IBAction func sliderMoved(_ slider: UISlider) {
+    @objc func sliderMoved(_ slider: UISlider) {
         model.currentValue = lroundf(slider.value)
     }
     func startNewRoundView() {
@@ -67,7 +73,7 @@ class ViewController: UIViewController {
         targetLabel.text = String(model.targetValue)
         roundCounter.text = String(model.round)
     }
-
+    
     @objc func startOverPressedView() {
         model.startOverPressed()
         scoreCounter.text = String(model.score)
@@ -76,7 +82,7 @@ class ViewController: UIViewController {
     }
     private func addConstraints() {
         var constraints = [NSLayoutConstraint]()
-        // Delete automatic constraints
+
         startOverButton.translatesAutoresizingMaskIntoConstraints = false
         scoreCounter.translatesAutoresizingMaskIntoConstraints = false
         scoreLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -85,37 +91,60 @@ class ViewController: UIViewController {
         roundLabel.translatesAutoresizingMaskIntoConstraints = false
         hitMe.translatesAutoresizingMaskIntoConstraints = false
         identifierButton.translatesAutoresizingMaskIntoConstraints = false
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label100.translatesAutoresizingMaskIntoConstraints = false
+        putYheBulls.translatesAutoresizingMaskIntoConstraints = false
+        targetLabel.translatesAutoresizingMaskIntoConstraints = false
         
-        // Add constraints
         constraints.append(startOverButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16))
         constraints.append(startOverButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16))
         
         constraints.append(scoreLabel.centerXAnchor.constraint(equalTo: startOverButton.centerXAnchor, constant: view.frame.width/4))
         constraints.append(scoreLabel.centerYAnchor.constraint(equalTo: startOverButton.centerYAnchor))
-
+        
         constraints.append(scoreCounter.centerXAnchor.constraint(equalTo: scoreLabel.trailingAnchor, constant: 20))
         constraints.append(scoreCounter.centerYAnchor.constraint(equalTo: scoreLabel.centerYAnchor))
         
         
         constraints.append(goID.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16))
         constraints.append(goID.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16))
-
+        
         constraints.append(roundCounter.centerXAnchor.constraint(equalTo: goID.centerXAnchor, constant: -view.frame.width/4))
         constraints.append(roundCounter.centerYAnchor.constraint(equalTo: scoreLabel.centerYAnchor))
-
+        
         constraints.append(roundLabel.trailingAnchor.constraint(equalTo: roundCounter.leadingAnchor, constant: -20))
         constraints.append(roundLabel.centerYAnchor.constraint(equalTo: scoreLabel.centerYAnchor))
-
+        
         constraints.append(hitMe.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0))
-        constraints.append(hitMe.bottomAnchor.constraint(equalTo: scoreCounter.topAnchor, constant: -50))
+        constraints.append(hitMe.bottomAnchor.constraint(equalTo: scoreCounter.topAnchor, constant: -20))
         
         constraints.append(identifierButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16))
         constraints.append(identifierButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 16))
+
+        constraints.append(slider.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(slider.centerYAnchor.constraint(equalTo: view.centerYAnchor))
+        constraints.append(slider.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75))
+        
+        constraints.append(label1.trailingAnchor.constraint(equalTo: slider.leadingAnchor, constant: -30))
+        constraints.append(label1.centerYAnchor.constraint(equalTo: view.centerYAnchor))
+        
+        constraints.append(label100.leadingAnchor.constraint(equalTo: slider.trailingAnchor, constant: 30))
+        constraints.append(label100.centerYAnchor.constraint(equalTo: view.centerYAnchor))
+        
+        constraints.append(putYheBulls.centerXAnchor.constraint(equalTo: view.centerXAnchor))
+        constraints.append(putYheBulls.bottomAnchor.constraint(equalTo: slider.topAnchor, constant: -70))
+        
+        constraints.append(targetLabel.leadingAnchor.constraint(equalTo: putYheBulls.trailingAnchor, constant: 30))
+        constraints.append(targetLabel.centerYAnchor.constraint(equalTo: putYheBulls.centerYAnchor))
+        
+        
+       
+        
         NSLayoutConstraint.activate(constraints)
     }
-
+    
     @objc func startOverAnimation() {
-        // добавляем анимацию на кнопку
         UIView.animate(withDuration: 0.5, animations: { [self] in
             self.startOverButton.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
         }, completion: { _ in
@@ -132,7 +161,7 @@ class ViewController: UIViewController {
         hitMe.layer.masksToBounds = true
         view.addSubview(hitMe)
         
-        goID.setTitle("GO ID", for: .normal)
+        goID.setTitle("Statistic", for: .normal)
         goID.backgroundColor = UIColor.blue
         goID.titleLabel?.textColor = UIColor.white
         goID.layer.cornerRadius = 10
@@ -162,7 +191,21 @@ class ViewController: UIViewController {
         view.addSubview(roundLabel)
         roundCounter.text = "1"
         view.addSubview(roundCounter)
+        putYheBulls.text = "Put the Bulls's eye as close as you can to:"
+        view.addSubview(putYheBulls)
+        label1.text = "1"
+        view.addSubview(label1)
+        label100.text = "100"
+        view.addSubview(label100)
+        targetLabel.text = "50"
+        view.addSubview(targetLabel)
     }
-
+    func sliderModifire() {
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.value = 50
+        view.addSubview(slider)
+    }
+    
 }
 
